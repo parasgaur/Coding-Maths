140308db068dfee5f0335f9702768d3e25ee3cc5
avesrastogi <55761079+avesrastogi@users.noreply.github.com>
2020-10-02 22:18:53 +0530

    omitted useless spaces and curly brace position

--- gcd.cpp
+++ gcd.cpp
@@ -6,16 +6,15 @@ using namespace std;
 int main()
 {
     int number1, number2;
-    cin >> number1 >> number2;
+    cin>>number1>>number2;
     
-    while(number1 != number2)
-    {
+    while(number1 != number2) {
         if(number1 > number2)
             number1 -= number2;
         else
             number2 -= number1;
     }
 
-    cout << "GCD = " << number1;
+    cout <<"GCD = "<<number1;
     return 0;
 }
